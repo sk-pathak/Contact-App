5bae31ba2a9e99114d9f51d9b39a937a10ad207a
sk-pathak <sumitpathak2002@gmail.com>
2024-06-18 17:33:00 +0530

    pushed to github

---
+++ contact-backend/src/main/java/org/openlake/contactapi/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package org.openlake.contactapi.repo;
+
+import org.openlake.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
---
+++ contact-backend/src/test/java/org/openlake/contactapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openlake.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
